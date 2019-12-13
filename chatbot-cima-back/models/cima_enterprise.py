94daee35e9b32fb51cc5df3da387d63abc6f3b22
DanielAlpiste <danielalpiste@gmail.com>
2019-12-13 12:22:05 -0500

    cambios sin marshmellow

--- chatbot-cima-back/models/cima_enterprise.py
+++ chatbot-cima-back/models/cima_enterprise.py
@@ -1,9 +1,5 @@
 from app import db, ma
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
-from marshmallow import Schema, fields, pre_load
-from marshmallow import validate
-from marshmallow import Schema, fields
-from marshmallow_validators.wtforms import from_wtforms
 from wtforms.validators import Email, Length
 from sqlalchemy import *
 
