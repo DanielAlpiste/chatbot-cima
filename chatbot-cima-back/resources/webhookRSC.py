917b4b29da888f42d0d54265aa5cd183d41714a3
DanielAlpiste <daniel.alpiste@pucp.pe>
2020-01-10 09:25:27 -0500

    cambios

--- chatbot-cima-back/resources/webhookRSC.py
+++ chatbot-cima-back/resources/webhookRSC.py
@@ -13,10 +13,6 @@ class Consult(Resource):
 		if not d:
 			response = {'user': 'No input data provided'}
 			return response, status.HTTP_400_BAD_REQUEST
-
-		ruc = ''
-		telefono = ''
-		msg = ''
 		
 		action = d.get('queryResult').get('action')
 
@@ -26,6 +22,7 @@ class Consult(Resource):
 		
 		#if (action == 'action-verificar-email'):
 
+
 		if(action == 'action-oferta'):
 			query_aprobado = "SELECT bs.document_number AS 'nro_documento', clo.period_in_months AS 'numero_cuotas_maxima', clo.withholding AS 'porcentaje_retencion', clo.base_interest_rate AS 'tasa_interes', clo.monthly_fee AS 'cuota_mensual_maxima',clo.maximum_capital_amount AS 'oferta_maxima' FROM business bs JOIN catalog ca ON bs.id = ca.business_id JOIN catalog_item ci ON ca.id = ci.catalog_id JOIN offer clo  ON ci.id = clo.catalog_item_id WHERE bs.document_number = '$RUC' AND clo.status ='OFFERED' ORDER BY clo.created_date ASC LIMIT 1"
 			query_preaprobado = "SELECT ruc AS 'nro_documento', business_name AS 'razon_social',suitable AS 'pre_califica', final_rate AS 'tasa_interes' FROM ibk_data WHERE ruc = '$RUC';"
@@ -34,7 +31,7 @@ class Consult(Resource):
 			result = db.engine.execute(query_aprobado.replace('$RUC',documento))
 			if (len(result) > 0):
 				msg = 'Cuentas con una oferta de hasta S/.' + result[0]['maximum_capital_amount']
-			else if:
+			else:
 				result = db.engine.execute(query_preaprobado.replace('$RUC',documento))
 				#si tiene una oferta preaprobada
 				if (len(result) > 0):
