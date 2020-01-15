2ce2abdd8fcd19768dfaa5b00f8c3430c1b200be
Lopez Pesantes <dlopezp@intercorp.com.pe>
2020-01-15 16:03:55 -0500

    fixes varios

--- chatbot-cima-back/controllers/preventaCTL.py
+++ chatbot-cima-back/controllers/preventaCTL.py
@@ -1,31 +1,19 @@
 import status
 from app import db
 
-def verificarNumeroDocumento(documento):
-	text = "SELECT b.document_type AS 'tipo_documento', b.document_number AS 'nro_documento', a.email FROM business b JOIN account_business ab on ab.business_id = b.id JOIN account a on a.id = ab.account_id JOIN user u on a.user_id = u.id WHERE document_number = '" + documento + "'"
-	query = db.engine.execute(text).first()
-	if query is None:
-		msg = "Usted no es cliente nuestro, quizá se equivoco de RUC, si no es asi, puede comunicarse con uno de nuestros ejecutivos."
-	else:
-		msg = "¡Perfecto! Ahora, ¿puedes decirme tu correo electronico?"
-	d = {}
-	d['fulfillmentText'] = msg
-	return d
+def verificarNumeroDocumento(ruc):
 
-def verificarEmail(documento, email):
-	text = "SELECT b.document_type AS 'tipo_documento', b.document_number AS 'nro_documento', a.email FROM business b JOIN account_business ab on ab.business_id = b.id JOIN account a on a.id = ab.account_id JOIN user u on a.user_id = u.id WHERE document_number = '" + documento + "' and a.email = '" + email + "'"
-	query = db.engine.execute(text).first()
-	if query is None:
-		msg = "Usted no es cliente nuestro, quizá se equivoco de email, si no es asi, puede comunicarse con uno de nuestros ejecutivos."
-	else:
-		msg = "¡Excelente! ¿Cuál es su duda el día de hoy?"
-	d = {}
-	d['fulfillmentText'] = msg
-	return d
+	persona = None
+	query = "SELECT business_name as 'razon_social', client_first_name as 'nombre', final_rate as 'tasa' FROM ibk_data where ruc = '$RUC'"
+	result = db.engine.execute(query.replace('$RUC',ruc)).first()
+	if result is not None:
+		persona = result
+	return persona
 
 def verificarOferta(ruc):
 	query_aprobado = "SELECT bs.document_number AS 'nro_documento', clo.period_in_months AS 'numero_cuotas_maxima', clo.withholding AS 'porcentaje_retencion', clo.base_interest_rate AS 'tasa_interes', clo.monthly_fee AS 'cuota_mensual_maxima',clo.maximum_capital_amount AS 'oferta_maxima' FROM business bs JOIN catalog ca ON bs.id = ca.business_id JOIN catalog_item ci ON ca.id = ci.catalog_id JOIN offer clo  ON ci.id = clo.catalog_item_id WHERE bs.document_number = '$RUC' AND clo.status ='OFFERED' ORDER BY clo.created_date ASC LIMIT 1"
 	query_preaprobado = "SELECT ruc AS 'nro_documento', business_name AS 'razon_social',suitable AS 'pre_califica', final_rate AS 'tasa_interes' FROM ibk_data WHERE ruc = '$RUC';"
+	query_registrado = "SELECT b.document_type AS 'tipo_documento',b.document_number AS 'nro_documento',a.email FROM business b JOIN account_business ab on ab.business_id = b.id JOIN account a on a.id = ab.account_id JOIN user u on a.user_id = u.id WHERE document_number = '$RUC'"
 
 	#si tiene una oferta aprobada
 	result = db.engine.execute(query_aprobado.replace('$RUC',ruc)).first()
@@ -36,10 +24,15 @@ def verificarOferta(ruc):
 		result = db.engine.execute(query_preaprobado.replace('$RUC',ruc)).first()
 		#si tiene una oferta preaprobada
 		if (result is not None):
-			msg = '¡Felicitaciones! Cuentas con una oferta preaprobada, ingresa/registrate a cima para validar tus datos y puedas acceder al crédito CIMA: https://cima.pe/credito-pos'
+			#si ya tiene una cuenta 
+			usuario = db.engine.execute(query_registrado.replace('$RUC',ruc)).first()
+			if (usuario is not None):
+				msg = '¡¡Felicidades¡¡ Tienes un crédito esperando por ti. Ingresa a https://cima.pe/login con tu cuenta para que puedas aceptar la oferta para que podamos proceder con tu desembolso'
+			else:
+				msg = '¡¡Felicidades¡¡ Tienes un crédito esperando por ti. Ingresa a Cima: https://cima.pe/credito-pos, regístrate y completa los pasos. El sistema te evaluará en línea y te mostrará las condiciones de tu crédito. De estar de acuerdo deberás aceptar la oferta para que podamos proceder con tu desembolso.'
 		else:
 			#si no tiene ninguna oferta
-			msg = 'Actualmente no cuentas con una oferta en CIMA, pronto nos estaremos comunicando contigo cuando tengas un crédito disponible con nosotros'
+			msg = 'Por ahora no cuentas con una oferta vigente; sin embargo agradecemos el interés y te invitamos a revisar la información dentro de los próximos 30 días, ya que el sistema volverá a evaluarte  de forma automática según tus últimos flujos de venta con VISA.'
 	
 	d = {}
 	d['fulfillmentText'] = msg
@@ -50,9 +43,9 @@ def problemaInscripcion(ruc):
 
 	result = db.engine.execute(query_registrado.replace('$RUC',ruc)).first()
 	if (result is not None):
-		msg = 'Recuerda ingresar correctamente tu correo: ' + result['email'] + ' (sin espacios) desde el siguiente link: https://cima.pe/login. En caso contrario puedes restaurar tu contraseña desde este link: https://cima.pe/forgot-password'
+		msg = 'Recuerda ingresar correctamente tu correo: ' + result['email'] + ' (sin espacios) desde el siguiente link: https://cima.pe/login. En caso no recuerdes tu contraseña deberás darle click a este link para que la puedas restaurar: https://cima.pe/forgot-password'
 	else:
-		msg = 'Al parecer no te has registrado en cima con el correo ' + result['email'] +'. Revisa que este sea el correo correcto (sin espacios). Caso contrario puedes registrarte desde https://cima.pe/credito-pos'
+		msg = 'Al parecer no estas ingresando con el correo registrado en CIMA (' + result['email'] +'). Revisa que este sea el correo correcto (sin espacios). Caso contrario puedes registrarte desde https://cima.pe/credito-pos'
 	
 	d = {}
 	d['fulfillmentText'] = msg
@@ -63,9 +56,9 @@ def problemaLogin(ruc):
 
 	result = db.engine.execute(query_registrado.replace('$RUC',ruc)).first()
 	if (result is not None):
-		msg = 'Recuerda ingresar correctamente tu correo: '+ result['email'] +'. En caso contrario puedes restaurar tu contraseña desde este link: https://cima.pe/forgot-password'
+		msg = 'Recuerda ingresar correctamente tu correo: '+ result['email'] +'. En caso no recuerdes tu contraseña deberás darle clic a este link para que la puedas restaurar: https://cima.pe/forgot-password'
 	else:
-		msg = 'Al parecer no te has registrado en cima con el correo '+ result['email'] +'. Revisa que este sea el correo correcto (sin espacios). Caso contrario puedes registrarte desde https://cima.pe/credito-pos'
+		msg = 'Al parecer no estas ingresando con el correo registrado en CIMA. Revisa que tu correo sea correcto (sin espacios). Caso contrario puedes registrarte desde https://cima.pe/credito-pos'
 	
 	d = {}
 	d['fulfillmentText'] = msg
@@ -76,9 +69,48 @@ def getEjecutiva(ruc):
 
 	result = db.engine.execute(query_ejecutiva.replace('$RUC',ruc)).first()
 	if (result is not None):
-		msg = 'Puedes comunicarte con tu ejecutivo(a) ' + result['nombre_ejecutiva'] + ' ' + result['apellido_ejecutiva'] + ' a este número: ' + result['telefono']
+		msg = 'Puedes comunicarte con tu ejecutivo(a) ' + result['nombre_ejecutiva'].capitalize() + ' ' + result['apellido_ejecutiva'] + ' a este número: ' + result['telefono']
 	else:
 		msg = 'Hola, la comunicación es por este medio. Si necesitas mas información primero regístrate en https://cima.pe/credito-pos para comunicarte con uno de nuestros ejecutivos'
+	
+	d = {}
+	d['fulfillmentText'] = msg
+	return d
+	
+def getEjecutivaSoporte(ruc):
+	query_ejecutiva = "SELECT b.document_type AS 'tipo_documento',b.document_number AS 'nro_documento',e.first_name AS 'nombre_ejecutiva',e.last_name AS 'apellido_ejecutiva',e.phone  AS 'telefono' FROM business b LEFT JOIN user_business ub on b.id = ub.business_id and ub.type_code = 'EXECUTIVE' LEFT JOIN user e on e.id = ub.user_id WHERE document_number = '$RUC'"
+
+	result = db.engine.execute(query_ejecutiva.replace('$RUC',ruc)).first()
+	if (result is not None):
+		msg = 'Parece que no logro entender lo que quieres decir. Puedes comunicarte con tu ejecutivo(a) ' + result['nombre_ejecutiva'].capitalize() + ' ' + result['apellido_ejecutiva'] + ' a este número: ' + result['telefono']
+	else:
+		msg = 'Parece que no logro entender lo que quieres decir. Un ejecutivo de negocio se comunicara a tu numero telefónico.'
+	
+	d = {}
+	d['fulfillmentText'] = msg
+	return d
+
+def getEjecutiva(ruc):
+	query_ejecutiva = "SELECT b.document_type AS 'tipo_documento',b.document_number AS 'nro_documento',e.first_name AS 'nombre_ejecutiva',e.last_name AS 'apellido_ejecutiva',e.phone  AS 'telefono' FROM business b LEFT JOIN user_business ub on b.id = ub.business_id and ub.type_code = 'EXECUTIVE' LEFT JOIN user e on e.id = ub.user_id WHERE document_number = '$RUC'"
+
+	result = db.engine.execute(query_ejecutiva.replace('$RUC',ruc)).first()
+	if (result is not None):
+		msg = 'Para cualquier consulta o duda podrás comunicarte con tu ejecutiva ' + result['nombre_ejecutiva'].capitalize() + ' ' + result['apellido_ejecutiva'] + ' a este número: ' + result['telefono']
+	else:
+		msg = 'Para cualquier consulta o duda podrás comunicarte al siguiente buzón: cima.interbank@intercorp.com.pe '
+	
+	d = {}
+	d['fulfillmentText'] = msg
+	return d
+
+def problemaProceso(ruc):
+	query_ejecutiva = "SELECT b.document_type AS 'tipo_documento',b.document_number AS 'nro_documento',e.first_name AS 'nombre_ejecutiva',e.last_name AS 'apellido_ejecutiva',e.phone  AS 'telefono' FROM business b LEFT JOIN user_business ub on b.id = ub.business_id and ub.type_code = 'EXECUTIVE' LEFT JOIN user e on e.id = ub.user_id WHERE document_number = '$RUC'"
+
+	result = db.engine.execute(query_ejecutiva.replace('$RUC',ruc)).first()
+	if (result is not None):
+		msg = 'Puedes intentar completar el proceso desde una PC o Laptop. Puedes comunicarte con tu ejecutivo(a) ' + 'Para cualquier consulta o duda podrás comunicarte con tu ejecutiva ' + result['nombre_ejecutiva'] + ' ' + result['apellido_ejecutiva'] + ' a este número: ' + result['telefono']
+	else:
+		msg = 'Puedes intentar completar el proceso desde una PC o Laptop. Si el problema persistiera podrás comunicarte al siguiente buzón: cima.interbank@intercorp.com.pe '
 	
 	d = {}
 	d['fulfillmentText'] = msg
--- chatbot-cima-back/resources/webhookRSC.py
+++ chatbot-cima-back/resources/webhookRSC.py
@@ -16,41 +16,74 @@ class Consult(Resource):
 		
 		action = d.get('queryResult').get('action')
 		resp = {}
+		ruc = ''
+		telefono = ''
 
-		if(d.get('queryResult').get('outputContexts')[0].get('parameters').get('documento.original') is None):
+
+		#validacion de datos RUC y TELEFONO
+		if (action == 'input.unknown'):
+			if (d.get('queryResult').get('outputContexts')[0].get('parameters') is None):
+				resp['fulfillmentText'] = 'El RUC/DNI que has ingresado no esta en formato correcto, recuerda que deben ser 11 u 8 dígitos. ¿Puedes ingresarlo nuevamente?'
+				return resp
+			if (d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original') is None):
+				resp['fulfillmentText'] = 'El teléfono que has ingresado no esta en formato correcto, recuerda que deben ser 9 dígitos. ¿Puedes ingresarlo nuevamente?'
+				return resp
+		
+		#si no se reconoce la intencion por segunda vez
+		if (action == 'input.unknown.2'):
+			if (d.get('queryResult').get('outputContexts')[0].get('parameters') is None):
+				resp['fulfillmentText'] = 'Parece que no logro entender lo que quieres decir. Un ejecutivo de negocio se comunicara a tu numero telefónico.'
+				return resp
+			if (d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original') is not None):
+				resp = preventaCTL.getEjecutiva(d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original'))
+
+
+		#Si no ha ingresado el numero de documento
+		if(d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original') is None):
+			#si me saluda, respondo el saludo y le pido el documento
 			if(action=='action-saludo'):
+				resp['fulfillmentText'] = "Hola, Bienvenido a CIMA soy tu asistente virtual que te ayudará a obtener tu préstamo rápido y fácil. \u000A Para poder comenzar necesitamos primero tu RUC o DNI"
 				resp['fulfillmentMessages'] = d.get('queryResult').get('fulfillmentMessages')
-				resp['fulfillmentMessages'][0]['text']['text'] = ['Hola! somos CIMA','Para poder comenzar necesitamos tu numero de documento']
+				resp['fulfillmentMessages'][0]['text']['text'] = ['Hola, Bienvenido a CIMA soy tu asistente virtual que te ayudará a obtener tu préstamo rápido y fácil.','Para poder comenzar necesitamos primero tu RUC o DNI']
+			#para el resto de intenciones pido el documento
 			else:
-				resp['fulfillmentText'] = 'Para poder comenzar necesitamos tu numero de documento'
-
+				resp['fulfillmentText'] = '¡Hola!. Antes de continuar necesitamos primero tu RUC o DNI'
 		else:
-			if(action=='action-documento'):
-				resp['fulfillmentText'] = 'Muy bien! Ahora cuentanos en que podemos ayudarte'
-				resp['outputContexts'] = d.get('queryResult').get('outputContexts')
-				resp['outputContexts'][0].get('parameters')['nombre'] = 'Diego'
-
-
-
-		#######################################################################################
-		if(action == 'action-numero-documento'):
+			#Si esta el documento pero no el telefono
 			ruc = d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original')
-			resp = preventaCTL.verificarNumeroDocumento(ruc)
 
-		if(action == 'action-oferta'):
-			ruc = d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original')
-			resp = preventaCTL.verificarOferta(ruc)
+			if(d.get('queryResult').get('outputContexts')[0].get('parameters').get('telefono.original') is None):
+				resp['fulfillmentText'] = 'Para darte una mejor atención también bríndanos tu número telefónico'
+			else:
+				#si ya estan seteado ambos despues de llenarlos, pongo frase de pregunta y seteo nombres
+				if(action in ['action-numero-documento','action-telefono']):
+					resp['fulfillmentText'] = '¡Entendido! Ahora cuéntanos ¿En que podemos ayudarte?'
+					persona = preventaCTL.verificarNumeroDocumento(ruc)
+					if persona is not None:
+						resp['outputContexts'] = d.get('queryResult').get('outputContexts')
+						resp['outputContexts'][0].get('parameters')['nombre'] = persona['nombre'].capitalize()
+						resp['outputContexts'][0].get('parameters')['razon_social'] = persona['razon_social'].title()
+						resp['outputContexts'][0].get('parameters')['tasa'] = persona['tasa']
 
-		if(action=='action-problema-inscripcion'):
-			ruc = d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original')
-			resp = preventaCTL.problemaInscripcion(ruc)
+				#si ya estan seteados ambos, proceso como normalmente lo haria
+				else:
+					
+					if(action == 'action-oferta'):
+						resp = preventaCTL.verificarOferta(ruc)
 
-		if(action=='action-problema-login'):
-			ruc = d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original')
-			resp = preventaCTL.problemaLogin(ruc)
+					if(action=='action-problema-inscripcion'):
+						resp = preventaCTL.problemaInscripcion(ruc)
 
-		if(action=='action-numero-telefonico'):
-			ruc = d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original')
-			resp = preventaCTL.getEjecutiva(ruc)
+					if(action=='action-problema-login'):
+						resp = preventaCTL.problemaLogin(ruc)
+
+					if(action=='action-numero-telefonico'):
+						resp = preventaCTL.getEjecutiva(ruc)
+
+					if(action=='action-problema-proceso'):
+						resp = preventaCTL.problemaProceso(ruc)
+						
+
+		#######################################################################################		
 
 		return  make_response(jsonify(resp))
