fb5b1f594441588ed942008f22f5fbf3e198daeb
Lopez Pesantes <dlopezp@intercorp.com.pe>
2020-01-20 17:37:14 -0500

    fix problemas contexto

--- chatbot-cima-back/resources/webhookRSC.py
+++ chatbot-cima-back/resources/webhookRSC.py
@@ -4,10 +4,21 @@ import status
 from sqlalchemy.exc import SQLAlchemyError
 import commons
 from app import db
+import sys
+
 
 from controllers import preventaCTL
 
 class Consult(Resource):
+
+	def getContexto(self,contextos):
+		c = None
+		for context in contextos:
+			url = context.get('name').split('/')
+			if url[-1] == 'datos-personales':
+				c = context
+		return c
+
 	def post(self):
 		d = request.get_json(force=True)
 		if not d:
@@ -15,73 +26,105 @@ class Consult(Resource):
 			return response, status.HTTP_400_BAD_REQUEST
 		
 		action = d.get('queryResult').get('action')
+		contexto = self.getContexto(d.get('queryResult').get('outputContexts'))
+
 		resp = {}
 		ruc = ''
 		telefono = ''
 
-
-		#validacion de datos RUC y TELEFONO
+		#Si no reconoce ninguna intencion
 		if (action == 'input.unknown'):
-			if (d.get('queryResult').get('outputContexts')[0].get('parameters') is None):
+
+			#Si no hay contexto, osea no ha ingresado ni RUC/Telefono
+			if contexto is None:
+				resp['fulfillmentText'] = "Hola, Bienvenido a CIMA soy tu asistente virtual que te ayudará a obtener tu préstamo rápido y fácil. \u000A Para poder comenzar necesitamos primero tu RUC o DNI"
+				return resp
+
+			print(contexto.get('parameters'), file=sys.stdout)
+
+			#No hay RUC por lo tanto no lo ingreso correctamente
+			if (contexto.get('parameters') is None or contexto.get('parameters').get('doc_number.original') is None):
 				resp['fulfillmentText'] = 'El RUC/DNI que has ingresado no esta en formato correcto, recuerda que deben ser 11 u 8 dígitos. ¿Puedes ingresarlo nuevamente?'
 				return resp
-			if (d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original') is None):
+
+			#No hay Telefono por lo tanto no lo ingreso correctamente
+			if (contexto.get('parameters').get('telefono.original') is None):
 				resp['fulfillmentText'] = 'El teléfono que has ingresado no esta en formato correcto, recuerda que deben ser 9 dígitos. ¿Puedes ingresarlo nuevamente?'
 				return resp
-		
-		#si no se reconoce la intencion por segunda vez
-		if (action == 'input.unknown.2'):
-			if (d.get('queryResult').get('outputContexts')[0].get('parameters') is None):
-				resp['fulfillmentText'] = 'Parece que no logro entender lo que quieres decir. Un ejecutivo de negocio se comunicara a tu numero telefónico.'
+
+			
+			#Si es una intencion sin reconocer pero ya tiene el RUC y el telefono es porque no se entendio la consulta del cliente
+			
+			#Aumenta contador de errores a 1
+			if(contexto.get('parameters').get('error') is None):
+				resp['outputContexts'] = [contexto]
+				resp['outputContexts'][0].get('parameters')['error'] = 1
+				resp['fulfillmentText'] = 'Creo que no entendí tu consulta, ¿Puedes decirla de otra manera?'
 				return resp
-			if (d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original') is not None):
-				resp = preventaCTL.getEjecutiva(d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original'))
+			
+			#en el contador de errores a 2
+			if(contexto.get('parameters')['error'] == 1):
+				resp['fulfillmentText'] = 'Lo sentimos parece que no podemos resolver tu duda por este medio. Una ejecutiva de negocio se estará comunicando contigo a tu telefono en la brevedad posible'
+				return resp
 
-
-		#Si no ha ingresado el numero de documento
-		if(d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original') is None):
-			#si me saluda, respondo el saludo y le pido el documento
-			if(action=='action-saludo'):
-				resp['fulfillmentText'] = "Hola, Bienvenido a CIMA soy tu asistente virtual que te ayudará a obtener tu préstamo rápido y fácil. \u000A Para poder comenzar necesitamos primero tu RUC o DNI"
-				resp['fulfillmentMessages'] = d.get('queryResult').get('fulfillmentMessages')
-				resp['fulfillmentMessages'][0]['text']['text'] = ['Hola, Bienvenido a CIMA soy tu asistente virtual que te ayudará a obtener tu préstamo rápido y fácil.','Para poder comenzar necesitamos primero tu RUC o DNI']
-			#para el resto de intenciones pido el documento
-			else:
-				resp['fulfillmentText'] = '¡Hola!. Antes de continuar necesitamos primero tu RUC o DNI'
+		#si hay alguna intencion reconocida
 		else:
-			#Si esta el documento pero no el telefono
-			ruc = d.get('queryResult').get('outputContexts')[0].get('parameters').get('doc_number.original')
 
-			if(d.get('queryResult').get('outputContexts')[0].get('parameters').get('telefono.original') is None):
-				resp['fulfillmentText'] = 'Para darte una mejor atención también bríndanos tu número telefónico'
-			else:
-				#si ya estan seteado ambos despues de llenarlos, pongo frase de pregunta y seteo nombres
-				if(action in ['action-numero-documento','action-telefono']):
+			#si me saluda 
+			if(action=='action-saludo'):
+
+				#pero no hay documento
+				if contexto is None or contexto.get('parameters').get('doc_number.original') is None:
+					resp['fulfillmentText'] = "Hola, Bienvenido a CIMA soy tu asistente virtual que te ayudará a obtener tu préstamo rápido y fácil. \u000A Para poder comenzar necesitamos primero tu RUC o DNI"
+
+				#si no hay telefono
+				elif contexto.get('parameters').get('telefono.original') is None:
+					resp['fulfillmentText'] = 'Para darte una mejor atención también bríndanos tu número telefónico'
+				
+				else:
+					resp['fulfillmentText'] = 'Hola ¿En qué podemos ayudarte?'
+				
+			#si ha ingreso su documento o telefono
+			elif(action in ['action-numero-documento','action-telefono']):
+
+				#pero no hay documento
+				if contexto is None or contexto.get('parameters').get('doc_number.original') is None:
+					resp['fulfillmentText'] = "Para poder comenzar primero necesitamos primero tu RUC o DNI"
+
+				#si no hay telefono
+				elif contexto.get('parameters').get('telefono.original') is None:
+					resp['fulfillmentText'] = 'Para darte una mejor atención también bríndanos tu número telefónico'
+				
+				#si ya ingreso correctamente ambos datos
+				else:
 					resp['fulfillmentText'] = '¡Entendido! Ahora cuéntanos ¿En que podemos ayudarte?'
 					persona = preventaCTL.verificarNumeroDocumento(ruc)
 					if persona is not None:
-						resp['outputContexts'] = d.get('queryResult').get('outputContexts')
+						resp['outputContexts'] = [contexto]
 						resp['outputContexts'][0].get('parameters')['nombre'] = persona['nombre'].capitalize()
 						resp['outputContexts'][0].get('parameters')['razon_social'] = persona['razon_social'].title()
 						resp['outputContexts'][0].get('parameters')['tasa'] = persona['tasa']
 
+
+		
+
 				#si ya estan seteados ambos, proceso como normalmente lo haria
-				else:
-					
-					if(action == 'action-oferta'):
-						resp = preventaCTL.verificarOferta(ruc)
+			else:
+				
+				if(action == 'action-oferta'):
+					resp = preventaCTL.verificarOferta(ruc)
 
-					if(action=='action-problema-inscripcion'):
-						resp = preventaCTL.problemaInscripcion(ruc)
+				if(action=='action-problema-inscripcion'):
+					resp = preventaCTL.problemaInscripcion(ruc)
 
-					if(action=='action-problema-login'):
-						resp = preventaCTL.problemaLogin(ruc)
+				if(action=='action-problema-login'):
+					resp = preventaCTL.problemaLogin(ruc)
 
-					if(action=='action-numero-telefonico'):
-						resp = preventaCTL.getEjecutiva(ruc)
+				if(action=='action-numero-telefonico'):
+					resp = preventaCTL.getEjecutiva(ruc)
 
-					if(action=='action-problema-proceso'):
-						resp = preventaCTL.problemaProceso(ruc)
+				if(action=='action-problema-proceso'):
+					resp = preventaCTL.problemaProceso(ruc)
 						
 
 		#######################################################################################		
